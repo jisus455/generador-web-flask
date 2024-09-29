875160322237d2c09f2ea8ffbc62ad56d3d75839
jisus455 <jesus.espi2544@gmail.com>
2024-09-28 21:16:23 -0300

    create pages generate and result

--- main.py
+++ main.py
@@ -4,36 +4,28 @@ from os import path
 app = Flask(__name__, template_folder="templates")
 app.secret_key = '1234'
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    # obtener el metodo que se esta utilizando
-    # if request.method == "GET":
-        # settear una variable de session en el servidor
-        # session["logged"] = True
-        # session["name"] = "jesus"        
-    return render_template("login.html")
+    if request.method == 'GET':      
+        return render_template("generate.html")
 
-    # if request.method == "POST":
-    #     # acceder a los valores enviados por formulario
-    #     request.form["username"]
-    #     request.form["password"]
-    #     error = 'sos un crack'
-    #     return render_template("login.html", error=error)
+    if request.method == 'POST':
+        genero = request.form["genero"]
+        dni = request.form["dni"]
+        return render_template("result.html", cuil=dni)    
 
-@app.route("/cuil", methods=['POST'])
-def data():
-    # acceder a los valores enviados por formulario
-    genero = request.form["genero"]
-    dni = request.form["dni"]
-    return render_template("login.html", cuil=f'23-{dni}-1')    
 
+@app.route("/generate.css")
+def styleGenerate():
+    return send_from_directory(path.join(app.root_path, 'static', 'css'), 'generate.css')
+
+@app.route("/result.css")
+def styleResult():
+    return send_from_directory(path.join(app.root_path, 'static', 'css'), 'result.css')
 
 @app.route("/favicon.ico")
 def icon():
     return send_from_directory(path.join(app.root_path, 'static', 'image'), 'baloncesto.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route("/login.css")
-def style():
-    return send_from_directory(path.join(app.root_path, 'static', 'css'), 'login.css')
 
 app.run(host="localhost", port=8080, debug=True)
